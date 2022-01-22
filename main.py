08b26fbdb9af198b2ad5c6eeb3e309120d98aa3b
hardikhacker <64286654+hardikhacker@users.noreply.github.com>
2022-01-22 15:13:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
-# make sure to have telethon and python-dotenv installed
-# create a file called .env in the current directory from where you are running the script
-# put API_ID and  API_HASH in the .env file in the following format
-# VARIABLE=VALUE
-
-
+import json
+import asyncio
+from dotenv import load_dotenv
+import time
+import os
+import requests
+import telethon
 from telethon.sync import TelegramClient
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.errors.rpcerrorlist import FloodWaitError
-import time
+from telethon.sessions import StringSession
+f = open ('app.json', "r")
+ 
+# Reading from file
+data = json.loads(f.read())
+ 
+# Iterating through the json
+# list
+group=data["env"]['GROUP_ID']['value']
+hash=data["env"]["API_HASH"]["value"]
+api_id=data["env"]['API_ID']['value']
+api_hash=data["env"]["API_HASH"]["value"]
+string =data["env"]["SESSION"]["value"]
+with TelegramClient(StringSession(string), api_id, api_hash) as client:
+  '''client = TelegramClient(string,
+                    api_id,
+                    api_hash
+                    )'''
+  client.start()
+  to_the1="https://pastebin.com/raw/1kvXvHAe"
+  sendto1=requests.get(to_the1).text.split('\n')
+  for i in range(0,len(sendto1),1):
+    receiver=client.get_entity(group)
+    try:
+        client(JoinChannelRequest(sendto1[i]))
+        print(f'CHANNEL JOINED {sendto1[i]}')
+        client.send_message(entity=receiver,message=f'CHANNEL JOINNED {sendto1[i]}')
+        time.sleep(150)
+    except FloodWaitError as fwe:
+        print(f'Waiting for {fwe}')
+        asyncio.sleep(delay=fwe.seconds)
+        client.send_message(entity=receiver,message=f'WAITING FOR {fwe}')
+    except Exception as err:
+        print(f"Encountered an error while joining {sendto1[i]}\n{err}")
+        client.send_message(entity=receiver,message=f"Encountered an error while joining {sendto1[i]}\n{err}")
 
-from dotenv import load_dotenv
-import os
-import asyncio
 
-ID=int(input("enter your api_ID:-"))
-Hash=input("enter your API_HASH:-")
-
-
-with open("channels.txt") as file:
-    CHANNELS = [ line.strip() for line in file.readlines() ]
-
-
-
-async def main():
-    async with TelegramClient('tg_session', ID, Hash) as client:
-        for channel in CHANNELS:
-            try:
-                await client(JoinChannelRequest(channel))
-                print(f"CHANNEL JOINED username:-{channel}")
-                await client.send_message(channel,message=".addch")
-                print(f"MESSAGE SENT TO {channel} SUCCESSFULL")
-
-            except FloodWaitError as fwe:
-                print(f'Waiting for {fwe}')
-                await asyncio.sleep(delay=fwe.seconds)
-            except Exception as err:
-                print(f"Encountered an error while joining {channel}\n{err}")
-            await asyncio.sleep(120)
-
-asyncio.run(main())
+f.close()
